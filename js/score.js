621fce86d112fd70d396ae32fdf58682eaedee4f
GodtierGD <par3ll3l@gmail.com>
2026-04-05 11:57:39 +0100

    test2

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-1*Math.pow(rank-1, 0.97) + 420) *
+    let score = (-1.03*Math.pow(rank-1, 0.97) + 420) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
